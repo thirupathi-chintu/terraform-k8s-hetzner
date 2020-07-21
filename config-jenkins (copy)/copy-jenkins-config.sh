c74cc7519b2b6b96147d50a44d9236a260aa8472
Thirupathi Peraboina <thirupathiperaboina@gmail.com>
2020-07-21 16:55:19 +0530

    Changes done with jenkins config files

---
+++ config-jenkins (copy)/copy-jenkins-config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+JENKINS_MASTER=$(kubectl get pods -n jenkins --selector=component=jenkins-jenkins-master -o json | jq -r '.items[0].metadata.name')
+
+# Go to jenkins dashboard and create pipeline
+
+kubectl cp jenkins/$JENKINS_MASTER:/var/jenkins_home/secrets/hudson.util.Secret jenkins-secrets/
+kubectl cp jenkins/$JENKINS_MASTER:/var/jenkins_home/secrets/master.key jenkins-secrets/
+kubectl cp jenkins/$JENKINS_MASTER:/var/jenkins_home/users/admin/config.xml jenkins-secrets/
+kubectl cp jenkins/$JENKINS_MASTER:/var/jenkins_home/credentials.xml jenkins-secrets/
+kubectl cp jenkins/$JENKINS_MASTER:/var/jenkins_home/jobs/ jenkins-jobs/
+
+# Create secrets to allow for decrypting stored configuration in the future (e.g. the Github API token)
+kubectl create secret generic jenkins-secrets --namespace jenkins --from-file=jenkins-secrets/master.key --from-file=jenkins-secrets/hudson.util.Secret
+
+# Create jenkins secret for storing credentials
+kubectl create secret generic jenkins-credentials --namespace jenkins --from-file=jenkins-secrets/credentials.xml
